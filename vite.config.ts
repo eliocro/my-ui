0083b233c400ab9acb5825e1aeb5ed8c56bc1a7d
Élio Cró <elio.cro@gmail.com>
2024-01-09 14:57:00 +0000

    Setup types output

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ rollupTypes: true })],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/lib.ts'),
+      name: 'My-UI',
+      fileName: 'my-ui',
       formats: ['es'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
     },
   },
 });
