09ed848584c96dadb171be43a28e0db25332878d
Élio Cró <elio.cro@gmail.com>
2024-01-09 14:18:28 +0000

    Setup library mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/lib.ts'),
+      formats: ['es'],
+    },
+  },
+});
